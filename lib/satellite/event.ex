e2df6db30fbe06a2935af41af29a2a35526ce361
Fabio Ticconi <fabio.ticconi@gmail.com>
2024-07-25 17:15:47 +0200

    adding unique id for each event

--- lib/satellite/event.ex
+++ lib/satellite/event.ex
@@ -1,9 +1,10 @@
 defmodule Satellite.Event do
   @derive Jason.Encoder
-  defstruct [:origin, :timestamp, :version, :type, :context, :payload]
+  defstruct [:id, :origin, :timestamp, :version, :type, :context, :payload]
 
   # TODO make validation for this
   @type t :: %__MODULE__{
+          id: Ecto.UUID.t(),
           origin: binary(),
           timestamp: binary(),
           version: integer(),
@@ -13,10 +14,11 @@ defmodule Satellite.Event do
         }
 
   def new(attrs) do
+    id = Map.get(attrs, :id) || Ecto.UUID.generate()
     origin = Map.get(attrs, :origin) || Application.fetch_env!(:satellite, :origin)
     timestamp = Map.get(attrs, :timestamp) || DateTime.utc_now()
 
-    default_values = %{origin: origin, timestamp: timestamp, version: 1}
+    default_values = %{id: id, origin: origin, timestamp: timestamp, version: 1}
 
     struct!(__MODULE__, default_values |> Map.merge(attrs))
   end
