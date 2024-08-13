fc3059a6b8117a3e465cb99d4c664245d36335ba
Lorenzo Lopez <lorenzo.lopez@intec.es>
2024-08-13 14:13:31 +0200

    Remove error on autogenerated files

---
+++ .dialyzer_ignore.exs
@@ -0,0 +1,13 @@
+[
+  {"lib/satellite/com/vorwerk/cleaning/orbital/v1/charge_state.ex", :call},
+  {"lib/satellite/com/vorwerk/cleaning/orbital/v1/cleaning_end.ex", :call},
+  {"lib/satellite/com/vorwerk/cleaning/orbital/v1/cleaning_start.ex", :call},
+  {"lib/satellite/com/vorwerk/cleaning/orbital/v1/cleaning_start/map.ex", :call},
+  {"lib/satellite/com/vorwerk/cleaning/orbital/v1/cleaning_start/run.ex", :call},
+  {"lib/satellite/com/vorwerk/cleaning/orbital/v1/cleaning_start/settings.ex", :call},
+  {"lib/satellite/com/vorwerk/cleaning/orbital/v1/docking_state.ex", :call},
+  {"lib/satellite/com/vorwerk/cleaning/orbital/v1/event.ex", :call},
+  {"lib/satellite/com/vorwerk/cleaning/orbital/v1/payload.ex", :call},
+  {"lib/satellite/com/vorwerk/cleaning/orbital/v1/robot_error.ex", :call},
+  {"lib/satellite/com/vorwerk/cleaning/orbital/v1/robot_notification.ex", :call},
+]
